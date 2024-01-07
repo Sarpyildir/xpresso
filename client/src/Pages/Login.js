924b56234874210f7e23a064f6e6f8d040dac79a
Sarpyildir <sarpyildir@gmail.com>
2024-01-07 23:20:12 +0300

    login page details

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -7,34 +7,56 @@ const Login = () => {
   const gridStyle = {
     display: "grid",
     gridTemplateColumns: "1fr 1fr",
-    backgroundColor: "blue",
   };
   const leftGridStyle = {
     backgroundColor: "green",
+    display: "grid",
+    placeItems: "center",
+    gridTemplateRows: "3fr 4fr",
   };
   const rightGridStyle = {
     display: "grid",
     gridTemplateColumns: "1fr",
-    gridTemplateRows: "2fr 5fr",
-    backgroundColor: "yellow",
+    gridTemplateRows: "2fr 6fr",
+    width: "50%",
+    margin: "20px",
   };
   const rightGridFormStyle = {
     display: "grid",
     gridTemplateColumns: "1fr",
-    gridTemplateRows: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
+    gridTemplateRows: "2fr 2fr 2fr 2fr 2fr 2fr 1fr 2fr",
     gap: "10px",
-    backgroundColor: "pink",
   };
+
   return (
-    <div style={{ backgroundColor: "red" }}>
+    <div>
       <div style={gridStyle}>
         <div style={leftGridStyle}>
-          <h1>Logo</h1>
-          <h2>Welcome</h2>
+          <img src="./coffee.png" alt="Logo of website" width={"200px"} />
+
+          <div
+            style={{
+              display: "grid",
+              placeItems: "center",
+              margin: "2em",
+              textAlign: "center",
+            }}
+          >
+            <h1> WELCOME TO XPESSO</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              convallis sem nec tortor ullamcorper, ut tincidunt nibh aliquet.
+              Curabitur consequat quis tellus at posuere. Sed vestibulum ornare
+              enim, id laoreet neque dapibus placerat. Sed nibh orci, vestibulum
+              at elementum eu, dictum vel sem. Proin tristique nisl ac leo
+              luctus, a lobortis mauris bibendum. Quisque vel lacus leo. Nunc
+              mauris erat, commodo eu interdum vel
+            </p>
+          </div>
         </div>
         <div style={rightGridStyle}>
           <div>
-            <p>Welcome to XPRESSO</p>
+            <p>REGISTER</p>
           </div>
           <form>
             <div style={rightGridFormStyle}>
@@ -70,9 +92,11 @@ const Login = () => {
               <div>
                 <input type="checkbox" name="rememberMe" />
                 <label>Remember me</label>
+                <input type="checkbox" name="termsAndConditions" required />
+                <label>Terms And Conditions</label>
               </div>
 
-              <input type="submit" value="Submit" />
+              <input type="submit" value="Register" />
             </div>
           </form>
         </div>
