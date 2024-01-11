a10c83cf4278bc0d3ce75545624e6f95740dd5fd
Sarpyildir <sarpyildir@gmail.com>
2024-01-12 00:57:00 +0300

    Add  checking user existence in registeration

--- server/src/api/controllers/registerController.js
+++ server/src/api/controllers/registerController.js
@@ -1,18 +1,24 @@
 import User from "../models/user.js";
-// TODO: check if user already exists with the same email or username
+
 async function registerUser(req, res) {
   try {
+    // Check if a user with the same email or username already exists
+    const existingUser = await User.findOne({
+      $or: [{ email: req.body.email }, { username: req.body.username }],
+    });
+
+    if (existingUser) {
+      // Check why the user already exists and return ao appropriate error message
+      if (existingUser.email === req.body.email) {
+        return res.status(400).json("Email already in use.");
+      } else if (existingUser.username === req.body.username) {
+        return res.status(400).json("Username already in use.");
+      }
+    }
+
     const newUser = new User(req.body);
-    await newUser
-      .save()
-      .then((savedUser) => {
-        console.log(savedUser);
-        res.status(201).json(savedUser);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json("Unable to save a new user" + err);
-      });
+    await newUser.save();
+    res.status(201).json(newUser);
   } catch (e) {
     console.log(e);
     res.status(500).json("Internal server error");
