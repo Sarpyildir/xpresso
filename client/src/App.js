b46575633ee96e95648c538ad6765ce5c566c871
Sarpyildir <sarpyildir@gmail.com>
2024-01-17 14:28:01 +0300

    Add Settings page and display user information

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 import LoginScreen from "./Pages/LoginScreen";
 import RegisterationScreen from "./Pages/RegisterationScreen";
-
+import Settings from "./Pages/Settings";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterationScreen />} />
+        <Route path="/settings" element={<Settings />} />
         <Route index path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- client/src/Pages/Settings.js
+++ client/src/Pages/Settings.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../Components/Header.js";
 import Footer from "../Components/Footer.js";
 import Button from "@mui/material/Button";
-
+// TODO: session storage ile userData'yı bağla.
 const Settings = () => {
   // Styles
   const containerStyle = {
@@ -11,11 +11,31 @@ const Settings = () => {
     height: "100vh",
     fontSize: "1rem",
   };
-
+  const storedUserData = JSON.parse(sessionStorage.getItem("user"));
+  const userData = {
+    username: "Sarpyildir",
+    name: "Sarp",
+    surname: "Yıldırım",
+    email: "sarpyildir@gmail.com",
+  };
   return (
     <div style={containerStyle}>
       <Header />
-      <div style={contentStyle}>Settings</div>
+      <div>
+        <h1>User Information</h1>
+        <div>
+          <p>Username: {userData.username} </p>
+        </div>
+        <div>
+          <p>Name: {userData.name} </p>
+        </div>
+        <div>
+          <p>Surname: {userData.surname} </p>
+        </div>
+        <div>
+          <p>Email: {userData.email} </p>
+        </div>
+      </div>
       <Footer />
     </div>
   );
